111cfca962b1bde402e4bb4758e5938ecf97a920
Chandra Prakash <pr.chandraprakash@gmail.com>
2022-10-17 21:46:39 +0530

    Show current screen in navigation

--- src/Components/additional components/Footer.js
+++ src/Components/additional components/Footer.js
@@ -4,19 +4,40 @@ import './Footer.css'
 
 function Footer(props) {
 
+  const handleClick = (event) => {
+    const target = event.target;
+    const div = target.closest('div');
+    const footer = target.closest('footer');
+
+    const curdiv = footer.querySelector('.active');
+    console.log(curdiv);
+    curdiv.classList.remove('active');
+
+    div.classList.add('active');
+  }
+
   return (
-    <footer>
-      <Link to="/">
-        <i className="fa fa-home fa-2xl"/>
-      </Link>
-      <Link to="/shop">
-        <i className="fa-solid fa-pills fa-2xl"/>
-      </Link>
-      <Link to='/lab'>
-        <i className="fa fa-flask fa-2xl"/>
-      </Link>
-      <Link to="/doctor">
-        <i className="fa fa-user-md fa-2xl"/></Link>
+    <footer onClick={handleClick}>
+      <div className="active">
+        <Link to="/">
+          <i className="fa fa-home fa-2xl"/>
+        </Link>
+      </div>
+      <div>
+        <Link to="/shop">
+          <i className="fa-solid fa-pills fa-2xl"/>
+        </Link>
+      </div>
+      <div>
+        <Link to='/lab'>
+          <i className="fa fa-flask fa-2xl"/>
+        </Link>
+      </div>
+      <div>
+        <Link to="/doctor">
+          <i className="fa fa-user-md fa-2xl"/>
+        </Link>
+      </div>
       <div className="shopping-bag">
         <Link to="/cart">
           <i className="fa fa-shopping-bag fa-2xl"/>
